f73ee6887ebe04fce4316a0bd3c9ce8c722b6ca9
valardohaeris2 <aconner5674@gmail.com>
2024-08-23 14:52:40 -0400

    add terraform variables

---
+++ main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.64.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                   = var.region
+  shared_credentials_files = var.shared_credentials_files
+  profile                  = "default"
+}
+
+resource "aws_instance" "ansible-controller" {
+  ami                         = var.ami
+  associate_public_ip_address = true
+  instance_type               = "t2.micro"
+  key_name                    = var.key_name
+  vpc_security_group_ids = var.vpc_security_group_ids
+  source_dest_check = true
+
+  tags = {
+    "Name" = "ansible-controller"
+  }
+  tags_all = {
+    "Name" = "ansible-controller"
+  }
+  user_data = file("./InstallAnsible.sh")
+
+
+
+  cpu_options {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 3000
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 125
+    volume_size           = 8
+    volume_type           = "gp3"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+    description = "AWS Region"
+    type = string
+    default = "us-east-2"
+}
+
+variable "shared_credentials_files" {
+    description = "Local AWS credentials file"
+    type = list(string)
+} 
+
+variable "ami" {
+    description = "AWS AMI ID"
+    type = string
+}
+
+variable "key_name" {
+    description = "AWS key-pair name to associate w/ the EC2 Instance"
+    type = string
+}
+
+variable "vpc_security_group_ids" {
+    description = "AWS securoty group id to attach to EC2 instance"
+    type = list
+}
+
