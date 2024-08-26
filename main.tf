3a2bb606449489d726467e2f4a4e515d7ec38082
Ashley Conner <108750120+valardohaeris2@users.noreply.github.com>
2024-08-26 15:33:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region                   = var.region
+  region                   = var.region 
   shared_credentials_files = var.shared_credentials_files
   profile                  = "default"
 }
